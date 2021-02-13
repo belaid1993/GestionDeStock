fc6223abbc7f6c9cb310da1e2941b702a3a73a81
belaid reda <abdelkadirbelaid@gmail.com>
2021-02-13 15:20:35 +0000

    initialisation de projet

---
+++ src/test/java/com/belaid/gestionDeStock/GestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belaid.gestionDeStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
