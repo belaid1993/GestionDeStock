f0a5c580d190010a4673a20a2614e34b44624dad
belaid reda <abdelkadirbelaid@gmail.com>
2021-02-13 19:13:26 +0000

    generer la configuration mysql et hibernate

---
+++ src/main/java/com/belaid/gestionDeStock/model/LigneCommandeFournisseur.java
@@ -0,0 +1,16 @@
+package com.belaid.gestionDeStock.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ligneCommandeFournisseur")
+public class LigneCommandeFournisseur extends AbstractEntity {
+}
