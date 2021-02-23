ab86529f22e84b71d06caa3a06482734cab08a3b
belaid reda <abdelkadirbelaid@gmail.com>
2021-02-23 20:24:12 +0000

    creation des exception personnalisé

---
+++ src/main/java/com/belaid/gestionDeStock/handlers/ErrorDto.java
@@ -0,0 +1,23 @@
+package com.belaid.gestionDeStock.handlers;
+
+import com.belaid.gestionDeStock.exception.ErrorCodes;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer httpCode;
+
+    private ErrorCodes code;
+
+    private String message;
+
+    private List<String> errors = new ArrayList<>();
+}
