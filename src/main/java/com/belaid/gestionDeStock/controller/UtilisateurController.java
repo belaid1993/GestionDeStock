71788d3b5d6b5a35508623bd9ebd41107ed879e1
belaid reda <abdelkadirbelaid@gmail.com>
2021-06-09 00:46:34 +0100

    Modification de Swagger Configuration

--- src/main/java/com/belaid/gestionDeStock/controller/UtilisateurController.java
+++ src/main/java/com/belaid/gestionDeStock/controller/UtilisateurController.java
@@ -3,12 +3,14 @@ package com.belaid.gestionDeStock.controller;
 import com.belaid.gestionDeStock.controller.api.UtilisateurApi;
 import com.belaid.gestionDeStock.dto.UtilisateurDto;
 import com.belaid.gestionDeStock.services.UtilisateurService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class UtilisateurController implements UtilisateurApi {
 
     private UtilisateurService utilisateurService;
@@ -30,6 +32,7 @@ public class UtilisateurController implements UtilisateurApi {
 
     @Override
     public List<UtilisateurDto> findAll() {
+        System.out.println("###########" + utilisateurService.findAll());
         return utilisateurService.findAll();
     }
 
